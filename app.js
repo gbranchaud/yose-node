6e6033cb36577cbd772cdd306590da806ddeb699
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-06 00:15:58 -0500

    possibly fixed the listen port issue.

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Live on heroku.\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
 console.log('Server running at localhost:' + port + '/');
