bdcc77b83009ab3044ffbd51c0472db9cc3f81a4
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-06 00:56:49 -0500

    added a link to the github repo containing the code on the main page.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var http = require('http');
 var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Yose\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<!DOCTYPE html>\n<html><body>Hello Yose<ul><li><pre><a href="https://github.com/gbranchaud/yose-node">github repo</a></pre></li></ul></body></html>');
 }).listen(port);
 
 console.log('Server running at localhost:' + port + '/');
