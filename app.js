d725407b69fd381a6991786e0d885c6f82be3ebe
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-06 00:24:56 -0500

    world 1 level 1 should pass

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Live on heroku.\n');
+  res.end('Hello Yose\n');
 }).listen(port);
 
 console.log('Server running at localhost:' + port + '/');
