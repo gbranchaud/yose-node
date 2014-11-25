e95cdf8fef568bd72ffbea12f423ebca15cd569d
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-25 00:28:38 -0500

    added a status 200 test.

--- test/world1-1.js
+++ test/world1-1.js
@@ -1,13 +1,21 @@
 var chai = require("chai");
+var expect = chai.expect;
 var sinon = require("sinon");
 var w1 = require("../world-one.js");
 
 describe("world 1-1", function () {
+  it("should return 200 OK", function () {
+    var res = {writeHead: sinon.spy(), end: sinon.spy()};
+    w1.one({}, res);
+
+    expect(res.writeHead.args[0][0]).to.equal(200);
+  });
+
   it("should return a response containing 'Hello Yose'", function () {
     var res = {writeHead: sinon.spy(), end: sinon.spy()};
     w1.one({}, res);
 
     var responseBody = res.end.args[0][0];
-    chai.expect(responseBody).to.contain('Hello Yose');
+    expect(responseBody).to.contain("Hello Yose");
   });
 });
