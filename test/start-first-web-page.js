08156c82faee00e90620a71163bcb79ee52e7c3b
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-25 23:12:43 -0500

    made use of the supertest module: simpler when handling async. Removed the chai & sinon libs (now unused).

--- test/start-first-web-page.js
+++ test/start-first-web-page.js
@@ -1,22 +1,15 @@
 var http = require("http");
-var chai = require("chai");
-var expect = chai.expect;
-var router = require("../router.js");
+var request = require("supertest");
+var router = require("../router");
 
 describe("start challenge - first web page", function () {
   var testServer = http.createServer(router.onRequest).listen(5001);
 
-  it("should return 200 OK", function () {
-    http.get("http://localhost:5001", function(res) {
-      expect(res.statusCode).equal(200);
-    });
+  it("should return a successful response containing 'Hello Yose'", function (done) {
+    request(testServer)
+      .get("/")
+      .expect(200)
+      .expect(/Hello Yose/, done);
   });
 
-  it("should return a response containing 'Hello Yose'", function () {
-    http.get("http://localhost:5001", function(res) {
-      res.once('data', function (chunk) {
-        expect(chunk.toString()).contain("Hello Yose");
-      });
-    });
-  });
 });
