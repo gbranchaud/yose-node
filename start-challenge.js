9f13cd65375223168da58b5d9e15f4af761c02df
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-26 00:50:49 -0500

    added support for the share challenge (link to repo). Added the cheerio dom lib & re-added chai.

--- start-challenge.js
+++ start-challenge.js
@@ -1,6 +1,6 @@
 exports.firstWebPage = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<!DOCTYPE html>\n<html><body>Hello Yose<ul><li><pre><a href="https://github.com/gbranchaud/yose-node">github repo</a></pre></li></ul></body></html>');
+  res.end('<!DOCTYPE html>\n<html><body>Hello Yose<ul><li><pre><a id="repository-link" href="https://github.com/gbranchaud/yose-node">github repo</a></pre></li></ul></body></html>');
 }
 
 exports.firstWebService = function (req, res) {
--- test/start-challenge-test.js
+++ test/start-challenge-test.js
@@ -1,11 +1,14 @@
 var http = require("http");
+var url = require("url");
 var request = require("supertest");
+var cheerio = require("cheerio");
+var chai = require("chai");
+var should = chai.should();
 var router = require("../router");
 
 var testServer = null;
 describe("start challenge", function () {
   before(function launchServer() {
-    console.log()
     testServer = http.createServer(router.onRequest).listen(5001);
   });
 
@@ -19,8 +22,8 @@ describe("start challenge", function () {
 
     it("should return a response containing 'Hello Yose' on /", function (done) {
       request(testServer)
-      .get("/")
-      .expect(/Hello Yose/, done);
+        .get("/")
+        .expect(/Hello Yose/, done);
     });
 
     it("should return 404 for non-existing endpoints such as /missing-page", function (done) {
@@ -33,15 +36,43 @@ describe("start challenge", function () {
   describe("first web service", function () {
     it("should return a successful json response on /ping", function (done) {
       request(testServer)
-      .get("/ping")
-      .expect(200)
-      .expect("Content-Type", "application/json", done);
+        .get("/ping")
+        .expect(200)
+        .expect("Content-Type", "application/json", done);
     });
 
     it("should return { 'alive': true } response on /ping", function (done) {
       request(testServer)
-      .get("/ping")
-      .expect('{ "alive" : true }', done);
+        .get("/ping")
+        .expect('{ "alive" : true }', done);
+    });
+  });
+
+  describe("share", function () {
+    it("should return an html page with a repo link on /", function (done) {
+      request(testServer)
+        .get("/")
+        .expect(/a id="repository-link"/, done);
+    });
+
+    it("repo link element should point to an absolute url", function (done) {
+      request(testServer)
+        .get("/")
+        .expect(function(response) {
+          var $ = cheerio.load(response.text);
+          $("a#repository-link").attr("href").should.equal("https://github.com/gbranchaud/yose-node");
+        })
+        .end(done);
+    });
+
+    it.skip("(SLOW!) url pointed-to by the repo link should contain YoseTheGame in its readme", function (done) {
+      request("https://github.com")
+        .get("/gbranchaud/yose-node")
+        .expect(function(response) {
+          var $ = cheerio.load(response.text);
+          $("#readme").text().should.contain("yosethegame");
+        })
+        .end(done);
     });
   });
 });
