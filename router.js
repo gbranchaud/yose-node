333ed6712b3882f7e7fdab7269e926adcbfe8afd
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-25 19:54:57 -0500

    changed the naming convention a bit.

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
-var w1 = require('./world-one');
+var startChallenge = require('./start-challenge');
 
 exports.onRequest = function (req, res) {
-  w1.one(req, res);
+  startChallenge.firstWebPage(req, res);
 };
--- test/start-first-web-page.js
+++ test/start-first-web-page.js
@@ -1,19 +1,19 @@
 var chai = require("chai");
 var expect = chai.expect;
 var sinon = require("sinon");
-var w1 = require("../world-one.js");
+var startChallenge = require("../start-challenge.js");
 
-describe("world 1-1", function () {
+describe("start challenge - first web page", function () {
   it("should return 200 OK", function () {
     var res = {writeHead: sinon.spy(), end: sinon.spy()};
-    w1.one({}, res);
+    startChallenge.firstWebPage({}, res);
 
     expect(res.writeHead.args[0][0]).to.equal(200);
   });
 
   it("should return a response containing 'Hello Yose'", function () {
     var res = {writeHead: sinon.spy(), end: sinon.spy()};
-    w1.one({}, res);
+    startChallenge.firstWebPage({}, res);
 
     var responseBody = res.end.args[0][0];
     expect(responseBody).to.contain("Hello Yose");
