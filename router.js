0bc42fb65656dba32dac35069d63f9e4144b95b8
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-11-25 23:51:53 -0500

    added test for 404 on invalid endpoints (+ added code for it) + added failing tests for the /ping endpoint (ignored for now)

--- router.js
+++ router.js
@@ -1,5 +1,17 @@
+var url = require("url");
 var startChallenge = require('./start-challenge');
 
-exports.onRequest = function (req, res) {
-  startChallenge.firstWebPage(req, res);
+exports.onRequest = function (request, response) {
+  var parsedUrl = url.parse(request.url);
+
+  if (parsedUrl.pathname === "/") {
+    startChallenge.firstWebPage(request, response);
+  }
+
+  fallbackOn404(request, response);
 };
+
+function fallbackOn404(request, response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.end("page not found.");
+}
---
+++ test/start-challenge-test.js
@@ -0,0 +1,47 @@
+var http = require("http");
+var request = require("supertest");
+var router = require("../router");
+
+var testServer = null;
+describe("start challenge", function () {
+  before(function launchServer() {
+    console.log()
+    testServer = http.createServer(router.onRequest).listen(5001);
+  });
+
+  describe("first web page", function() {
+    it("should return a successful html response on /", function (done) {
+      request(testServer)
+        .get("/")
+        .expect(200)
+        .expect("Content-Type", "text/html", done);
+    });
+
+    it("should return a response containing 'Hello Yose' on /", function (done) {
+      request(testServer)
+      .get("/")
+      .expect(/Hello Yose/, done);
+    });
+
+    it("should return 404 for non-existing endpoints such as /missing-page", function (done) {
+      request(testServer)
+        .get("/missing-page")
+        .expect(404, done);
+    });
+  });
+
+  describe.skip("first web service", function () {
+    it("should return a successful json response on /ping", function (done) {
+      request(testServer)
+      .get("/ping")
+      .expect(200)
+      .expect("Content-Type", "application/json", done);
+    });
+
+    it("should return { 'alive': 'true' } response on /ping", function (done) {
+      request(testServer)
+      .get("/ping")
+      .expect('{ "alive" : "true" }', done);
+    });
+  });
+});
